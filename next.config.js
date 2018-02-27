902488c1255dfebf0d7a1452cfba90a02305e637
Estela <diazsanchez.estela@gmail.com>
2018-02-26 17:50:08 -0800

    reverting config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS({
-  cssModules: true
-})
-
-{
+module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' },
